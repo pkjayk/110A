a3b029ad7225b5c681e188fdbd3bda62ac3a2104
Jayson Kadlecek <jayson@propelplm.com>
2017-11-06 13:15:28 -0800

    Add some tests (failing

--- src/user_tests.py
+++ src/user_tests.py
@@ -6,6 +6,14 @@ class TestUserFunctions(unittest.TestCase):
 	def setUp(self):
 		self.user = User()
 
-	def testCheckCredentials():
+	def testInvalidEmail(self):
+		self.assertFalse(self.user.isValidEmail('invalidemail@email'))
 		
+	def testValidEmail(self):
+		self.assertTrue(self.user.isValidEmail('bob.moore@gmail.com'))
 
+	def testNotIsEmpty(self):
+		self.assertFalse(self.user.isEmpty('  j. ', 'j.'))
+
+	def testIsEmpty(self):
+		self.assertTrue(self.user.isEmpty('      ', ' '))
